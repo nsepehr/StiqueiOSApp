94685c0d0eba45a3c8f71298b4d2f549a8ea2309
Soheil <ysoheil@gmail.com>
2016-03-15 16:03:11 -0700

    Initial Commit

---
+++ Stique/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Stique
+//
+//  Created by Soheil Yasrebi on 3/15/16.
+//  Copyright © 2016 StiqueApp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
