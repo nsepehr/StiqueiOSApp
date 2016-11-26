1649025a0989a990a8d61a712ea0587e45a40895
Nima Sepehr <nsepehr@gmail.com>
2016-11-26 13:14:21 -0800

    Changed the credit page style and image

--- Stique/ShoppingController.swift
+++ Stique/ShoppingController.swift
@@ -8,65 +8,27 @@
 
 import UIKit
 
-class ShoppingController: UITableViewController {
+class ShoppingController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        navigationItem.title = "Shopping"
+        navigationItem.title = "Credits"
         
-        tableView.separatorColor = UIColor(netHex: 0x01c0ad)
-        tableView.backgroundColor = UIColor(netHex: 0x01c0ad)
         
-        let type = UILabel()
-        type.text = "How to earn free credit?"
-        type.font = UIFont.systemFontOfSize(16)
-        type.textAlignment = NSTextAlignment.Center
-//        view.addSubview(type)
+        let creditImage = UIImage(named: "credits")
+        let backgroundImage = UIImage(named: "credits_background")
+        
+        let backgroundImageView = UIImageView(image: backgroundImage)
+        backgroundImageView.sizeToFit()
+        backgroundImageView.heightPercent = 100
+        backgroundImageView.widthPercent = 100
+        view.addSubview(backgroundImageView)
+        
+        let creditImageView = UIImageView(image: creditImage)
+        creditImageView.sizeToFit()
+        creditImageView.frame.origin = CGPointMake(60, 150)
+        view.addSubview(creditImageView)
 
-        type.widthPercent = 100
-        type.marginTopAbsolute = view.bounds.height / 2.0
-        type.height = 50
-        
-        let money1 = UIButton()
-//        money1.setAttributedTitle(NSAttributedString(string: String.fontAwesomeIconWithName(.Money), attributes: [NSFontAttributeName: UIFont.fontAwesomeOfSize(35)]), forState: .Normal)
-        money1.setImage(UIImage(named: "1credit"), forState: .Normal)
-
-        view.addSubview(money1)
-        
-        money1.widthPercent = 40
-        money1.marginTopAbsolute = 75
-        money1.marginRightAbsolute = 60
-//        money1.height = 50
-        
-        let money2 = UIButton()
-        money2.setAttributedTitle(NSAttributedString(string: String.fontAwesomeIconWithName(.Money), attributes: [NSFontAttributeName: UIFont.fontAwesomeOfSize(35)]), forState: .Normal)
-        money2.setImage(UIImage(named: "2credit"), forState: .Normal)
-        view.addSubview(money2)
-        
-        money2.widthPercent = 40
-        money2.marginTopAbsolute = 75
-        money2.marginLeftAbsolute = 60
-//        money2.height = 50
-        
-        let money3 = UIButton()
-        money3.setAttributedTitle(NSAttributedString(string: String.fontAwesomeIconWithName(.Money), attributes: [NSFontAttributeName: UIFont.fontAwesomeOfSize(35)]), forState: .Normal)
-        money3.setImage(UIImage(named: "3credit"), forState: .Normal)
-        view.addSubview(money3)
-        
-        money3.widthPercent = 40
-        money3.marginTop = 75
-        money3.marginLeftAbsolute = 60
-//        money3.height = 50
-        
-        let money4 = UIButton()
-        money4.setAttributedTitle(NSAttributedString(string: String.fontAwesomeIconWithName(.Money), attributes: [NSFontAttributeName: UIFont.fontAwesomeOfSize(35)]), forState: .Normal)
-        money4.setImage(UIImage(named: "4credit"), forState: .Normal)
-        view.addSubview(money4)
-        
-        money4.widthPercent = 40
-        money4.marginTop = 75
-        money4.marginLeftAbsolute = 60
-//        money4.height = 50
     }
 }
