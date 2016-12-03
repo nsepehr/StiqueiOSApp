98ce20b5bc22d7abba342017a5f8f6da555ae5be
Nima Sepehr <nsepehr@gmail.com>
2016-12-03 01:21:05 -0800

    Added the filter page

--- Stique/FilterController.swift
+++ Stique/FilterController.swift
@@ -19,100 +19,64 @@ class FilterController: UITableViewController {
     
     let userDefaults = NSUserDefaults.standardUserDefaults()
     
-    let tableData = [
-        [
-            "name":"Standardized Exams",
-            "items":[["name":"GER GMAT LSAT"]]
-        ],[
-            "name":"Alphabetical Sorting",
-            "items":[["name":"Ascending"],["name":"Descending"]]
-        ],[
-            "name":"Filter By",
-            "items":[["name":"Watched"]]
-        ]
-    ]
+    // UI Outlet Object
+    @IBOutlet weak var ascendingCheck: UIButton!
+    @IBOutlet weak var descendingCheck: UIButton!
+    @IBOutlet weak var watchedCheck: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         navigationItem.title = "Filter Page"
+        ascendingCheck.hidden = true
+        descendingCheck.hidden = true
+        watchedCheck.hidden = true
     }
     
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let kLCellIdentifier = "customCell"
-        var cell = tableView.dequeueReusableCellWithIdentifier(kLCellIdentifier)
-        if cell == nil {
-            if indexPath.section == TableSections.StandardizeExams.rawValue && indexPath.row == 0 {
-                cell = ThreeImageCellView(style:.Default, reuseIdentifier: kLCellIdentifier)
-            } else {
-                cell = UITableViewCell(style:.Default, reuseIdentifier: kLCellIdentifier)
+    override func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+        // Below we will handle having a checkmark based on users previous selection of sorting and filters
+        // Filter is easy: if there's a filer watched set in userDefaults, we checkmark the cell
+        // Sorting is grouped:
+        //    If the sorting is Ascending first cell index must be checkmarked
+        //    else the Decending cell index (second one) should be checkmarked
+        if indexPath.section == TableSections.StandardizeExams.rawValue {
+            // Do nothing
+        } else if indexPath.section == TableSections.Sortings.rawValue {
+            if (indexPath.row == 0 && !userDefaults.boolForKey("sort")) {
+                ascendingCheck.hidden = false
+                descendingCheck.hidden = true
+            } else if (indexPath.row == 1 && userDefaults.boolForKey("sort")) {
+                ascendingCheck.hidden = true
+                ascendingCheck.hidden = false
             }
-        }
-        let myItem = rowsInSection(indexPath.section)[indexPath.row]
-        if indexPath.section == TableSections.StandardizeExams.rawValue && indexPath.row == 0 {
         } else {
-            // Below we will handle having a checkmark based on users previous selection of sorting and filters
-            // Filter is easy: if there's a filer watched set in userDefaults, we checkmark the cell
-            // Sorting is grouped: 
-            //    If the sorting is Ascending first cell index must be checkmarked
-            //    else the Decending cell index (second one) should be checkmarked
-            cell?.textLabel?.text = myItem["name"] as? String
-            if (indexPath.section == TableSections.Sortings.rawValue) {
-                if (indexPath.row == 0 && !userDefaults.boolForKey("sort")) {
-                    cell?.accessoryType = .Checkmark
-                } else if (indexPath.row == 1 && userDefaults.boolForKey("sort")) {
-                    cell?.accessoryType = .Checkmark
-                }
-            } else if (indexPath.section == TableSections.Filters.rawValue) {
-                if (userDefaults.boolForKey("watched")) {
-                    cell?.accessoryType = .Checkmark
-                }
+            if (userDefaults.boolForKey("watched")) {
+                watchedCheck.hidden = false
+            } else {
+                watchedCheck.hidden = true
             }
         }
-        
-        return cell!
     }
+    
     
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         tableView.deselectRowAtIndexPath(indexPath, animated: true)
         
-        let cell = tableView.cellForRowAtIndexPath(indexPath)!
         if (indexPath.section == TableSections.Sortings.rawValue) {
-            var otherCell: UITableViewCell!
             if (indexPath.row == 0) {
-                otherCell = tableView.cellForRowAtIndexPath(NSIndexPath(forRow: 1, inSection: indexPath.section))
                 userDefaults.setBool(false, forKey: "sort")
+                ascendingCheck.hidden = false
+                descendingCheck.hidden = true
             }
             if (indexPath.row == 1) {
-                otherCell = tableView.cellForRowAtIndexPath(NSIndexPath(forRow: 0, inSection: indexPath.section))
                 userDefaults.setBool(true, forKey: "sort")
+                ascendingCheck.hidden = true
+                descendingCheck.hidden = false
             }
-            // Below will check the selected cell and uncheck the other one in the group. We only have two cells so above harcoding we can deal with
-            cell.accessoryType = .Checkmark
-            otherCell.accessoryType = .None
-        }
-        if (indexPath.section == TableSections.Filters.rawValue) {
-            if (cell.accessoryType == .Checkmark) {cell.accessoryType = .None}
-            else {cell.accessoryType = .Checkmark}
+        } else if (indexPath.section == TableSections.Filters.rawValue) {
+            watchedCheck.hidden = !watchedCheck.hidden
             userDefaults.setBool(!userDefaults.boolForKey("watched"), forKey: "watched")
         }
         userDefaults.synchronize()
     }
-    
-    func rowsInSection(section: Int) -> [StiqueData] {
-        return (tableData[section]["items"] as! [StiqueData])
-    }
-    
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return tableData.count
-    }
-    
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return rowsInSection(section).count
-    }
-    
-    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return tableData[section]["name"] as? String
-    }
-    
 }
