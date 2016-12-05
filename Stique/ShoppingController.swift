8b8df8588bc6f425aa07079049ca6d3be32233ab
Nima Sepehr <nsepehr@gmail.com>
2016-12-04 22:22:25 -0800

    Added shopping page

--- Stique/ShoppingController.swift
+++ Stique/ShoppingController.swift
@@ -10,25 +10,12 @@ import UIKit
 
 class ShoppingController: UIViewController {
     
+    // UI Outlet Object
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        navigationItem.title = "Credits"
-        
-        
-        let creditImage = UIImage(named: "credits")
-        let backgroundImage = UIImage(named: "credits_background")
-        
-        let backgroundImageView = UIImageView(image: backgroundImage)
-        backgroundImageView.sizeToFit()
-        backgroundImageView.heightPercent = 100
-        backgroundImageView.widthPercent = 100
-        view.addSubview(backgroundImageView)
-        
-        let creditImageView = UIImageView(image: creditImage)
-        creditImageView.sizeToFit()
-        creditImageView.frame.origin = CGPointMake(60, 150)
-        view.addSubview(creditImageView)
 
+        backgroundImageView.alpha = 0.5
     }
 }
